7f8e5475b31a54ae73b684a720b659800a754bcd
Frank Warnaar <frank@voorhoede.nl>
2023-07-18 17:11:52 +0200

    basic theming setup

---
+++ src/components/product-cards.svelte
@@ -0,0 +1,91 @@
+<script>
+  import products from '../data/products.js';
+</script>
+
+<section class="root">
+  <div class="content">
+    <h2>New items</h2>
+    <ul class="list">
+      {#each products as product}
+        <li key={product.id} class="item">
+          <div class="image-wrapper">
+            <img src={product.images[0]} alt={product.title} class="image" />
+          </div>
+          <div class="item-body">
+            <h3 class="item-title">{product.title}</h3>
+            <p>€{product.price}</p>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</section>
+
+<style scoped>
+  .root {
+    padding: var(--spacing-2) 0;
+    background-color: var(--color-background);
+    color: var(--color-body);
+    font-family: var(--font-family);
+  }
+  
+  .content {
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+
+  .list {
+    padding: 0;
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+    width: 100%;
+  }
+
+  @media (min-width: 720px) {
+    .list {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+  
+  @media (min-width: 968px) {
+    .list {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  .item {
+    display: block;
+    width: 100%;
+    color: var(--color-body);
+    background-color: var(--color-gray);
+    border-radius: var(--border-radius-default);
+    overflow: hidden;
+  }
+
+  .image-wrapper {
+    aspect-ratio: 3 / 4;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: white;
+  }
+
+  .image {
+    max-width: 100%;
+    max-height: 60%;
+  }
+
+  .item-body {
+    padding: var(--spacing-1);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-1);
+  }
+
+  .item-title {
+    margin: 0;
+    color: var(--color-primary);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import ProductCards from "../components/product-cards.svelte";
+
+</script>
+
+<div class="theme-dark">
+  <ProductCards />
+</div>
+<div class="theme-light">
+  <ProductCards />
+</div>
+
+<style>
+  /* Theming variables */
+  .theme-dark {
+    --color-primary: #ce0b54;
+    --color-background: #333;
+    --color-body: #fff;
+    --color-gray: #555555;
+    --border-radius-default: 0.25rem;
+    --spacing-factor: 1;
+    --font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+  
+  .theme-light {
+    --color-primary: #ce0b54;
+    --color-background: #f5f5f5;
+    --color-body: #333;
+    --color-gray: #e0e0e0;
+    --border-radius-default: 0.25rem;
+    --spacing-factor: 2;
+    --font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  /* Unthemed */
+  :root {
+    --spacing-factor: 1;
+    --spacing-1: calc(1 * var(--spacing-factor) * 1rem);
+    --spacing-2: calc(1 * var(--spacing-factor) * 1.5rem);
+    --spacing-3: calc(1 * var(--spacing-factor) * 2rem);
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h3, p) {
+    margin: 0;
+  }
+</style>
